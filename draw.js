9f5bbf5cf0314790831d6e87c72d54dd0bdea820
Student <j.tauler2021@jamesrumsey.net>
2022-04-13 13:59:20 -0400

    j

--- draw.js
+++ draw.js
@@ -1,60 +1,99 @@
 // Classes
-class Triangle {
-    constructor() {
-        this.points = []
+class Triangle
+{
+    constructor()
+    {
+        this.points = [
+            new Point(CursorPosition, this),
+            new Point([CursorPosition-50,CursorPosition-100], this),
+            new Point([CursorPosition+50,CursorPosition-100], this)
+        ]
         this.right = false
     }
 
-    push([x, y]) {
-        this.points.push(new Point([x, y]))
-        if (this.points.length > 3) {
+    push([x, y])
+    {
+        this.points.push(new Point([x, y], this))
+        if (this.points.length > 3)
+        {
             this.points.shift()
         }
     }
 
-    sides() {
+    sides()
+    {
         let list = []
-        for (let p of this.points) {
+        for (let p of this.points)
+        {
             list.push(p.side.length)
         }
         return list
     }
 
     // Arrange points so that biggest is first. First point is hypot's.
-    arrange() {
-        this.points = this.points.sort((a, b) => b.side.length - a.side.length
-        )
+    // We need to leave ADJ and HYP where they lay, but must have HYP first.
+    arrange()
+    {
+        // this.points = this.points.sort((a, b) => b.side.length - a.side.length
+        // )
+        let biggest = null
+        for (let i of this.points) {
+            if (biggest == null || i.side.length > biggest.side.length) {
+                biggest = i
+            }
+        }
+        // Remove Hyp and place at front of array
+        this.points.splice(biggest.i, 1)
+        console.log(this.points)
+        this.points.unshift(biggest)
     }
-
 }
 
-class Point {
-    constructor([x, y]) {
+class Point
+{
+    constructor([x, y], triangle)
+    {
+        this.triangle = triangle
         this.x = x
         this.y = y
         this.angle = 0
         this.side = {
             "length": 0,
             "center": [0, 0],
-            "hypo": false
+            "hypo": false,
+            "midpoint": [0,0]
         }
     }
+
     set position(xy)
     {
+        // if (this.last)
+        // {
+        //     this.last = structuredClone(this)
+        // }
         [this.x, this.y] = xy
     }
+
     get position()
     {
         return [this.x, this.y]
     }
+
+    // Find point's index
+    get i()
+    {
+        return this.triangle.points.findIndex((p) => Object.is(p, this))
+    }
 }
 
 var Closest = {
-    triangle: new Triangle(),
-    point: new Point([null, null]),
-    pointi: function () {
-        return this.triangle.points.findIndex((p) => Object.is(p, this.point))
-    } // Find closest point's index
+    triangle: null,
+    point: null
+
+    // // // Find closest point's index
+    // get pointi () {
+    //     return this.point.i(this.triangle)
+    // }
 }
 
 let KEY = {
@@ -76,12 +115,14 @@ let KEY = {
 }
 
 // One off functions
-function round(value, precision) {
+function round(value, precision)
+{
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
 }
 
-function getDistance(x1, y1, x2, y2) {
+function getDistance(x1, y1, x2, y2)
+{
     let y = x2 - x1;
     let x = y2 - y1;
 
@@ -89,77 +130,97 @@ function getDistance(x1, y1, x2, y2) {
 }
 
 // DEBUG
-function DoNothing(s) {
+function DoNothing(s)
+{
     return 0
 }
 
 
-function Degrees(x) {
+function Degrees(x)
+{
     return x * (180 / Math.PI)
 }
-function Radians(x) {
+
+function Radians(x)
+{
     return x * (Math.PI / 180)
 }
 
-function MoveTriangle(tri) {
+function MoveTriangle(tri)
+{
     // Find diff between old and new loc
     let x = CursorPosition[0] - Closest.point.x
     let y = CursorPosition[1] - Closest.point.y
     // Move all points by the diff
-    for (let point of tri.points) {
+    for (let point of tri.points)
+    {
         let nx = point.x + x
         let ny = point.y + y
         point.position = [nx, ny]
     }
 }
 
-function SetClosest() {
+var last_i = 1
+
+function SetClosest()
+{
+    CurrentTriangle = Closest.triangle
     let tri = Closest.triangle
     if (tri.right) // Oblique
     {
-        tri.arrange()
-        let i = Closest.pointi()
-        console.log(i)
+        // tri.arrange()
+        let i = Closest.point.i
         if (i === 0) // If moving HYP
         {
             MoveTriangle(tri)
-        }
-        else
+        } else // ADJ / OPP
         {
             Closest.point.position = CursorPosition
+
             // Now we need to advance sister point 90 degrees from where we are now
-            let sis = ((i === 1) ? 2 : 1)
+            let sis = 1
+            let dir = 1
+            sis = ((i === 1) ? 2 : 1)
+            dir = ((i === 1) ? -1 : 1) * Radians(90) // + or - 90 degrees depending on rather selected point is opp or adj
 
             let a = tri.points[i]
             let b = tri.points[0]
             let c = tri.points[sis]
 
-            var angle = Math.atan2(a.y - b.y, a.x - b.x) // get angle of selected point
-            // + or - 90 degrees depending on rather selected point is opp or adj
-            angle += ((i === 1) ? -1 : 1) * Radians(90)
+            Closest.triangle.points[sis].position = AroundPoint([a, b, c], dir)
 
-            var x = b.x - c.x
-            var y = b.y - c.y
-            var r = Math.sqrt(x * x + y * y)
-            
-            let X = b.x + (Math.cos(angle) * r)
-            let Y = b.y + (Math.sin(angle) * r)
-
-            Closest.triangle.points[sis].position = [X,Y]
-
-            
-
+            last_i = i
         }
-    }
-    else
+    } else
     {
         Closest.point.position = CursorPosition
     }
 }
 
+function AroundPoint([a, b, c], dir)
+{
+    var angle = Math.atan2(a.y - b.y, a.x - b.x) // get angle of starting point
+    angle += dir
+
+    var x = b.x
+    var y = b.y
+    if (c)
+    {
+        x = b.x - c.x
+        y = b.y - c.y
+    }
+
+    var r = Math.sqrt(x * x + y * y)
+
+    let X = b.x + (Math.cos(angle) * r)
+    let Y = b.y + (Math.sin(angle) * r)
+    return [X, Y]
+}
+
 //// Equation Functions
 // COS
-function COS(triangle) {
+function COS(triangle)
+{
     let WorkLines = []
     // These letters do NOT correlate to the ones shown on the graph.
     let [a, b, c] = triangle.sides()
@@ -191,7 +252,8 @@ function COS(triangle) {
     return Degrees(v[0])
 }
 
-function SIN(triangle) {
+function SIN(triangle)
+{
     let [a, b, c] = triangle.sides()
     let hypot_angle = triangle.points[0].angle
     // let WorkLines = []
@@ -208,41 +270,54 @@ function SIN(triangle) {
     return Degrees(Math.asin(b / a)) // opp/hyp
 }
 
-function TAN(triangle) {
+function TAN(triangle)
+{
 
 }
 
 // Functions
-function GetCursorPosition(canvas, event) {
+function GetCursorPosition(canvas, event)
+{
     let rect = canvas.getBoundingClientRect()
     let x = event.clientX - rect.left
     let y = event.clientY - rect.top
     CursorPosition = [x, y]
 }
 
-function DrawSelector(Triangles) {
-    for (let tri of Triangles) {
-        for (let i in tri.points) {
+function DrawSelector(Triangles)
+{
+    for (let tri of Triangles)
+    {
+        console.log(tri)
+        for (let i in tri.points)
+        {
             // console.log(point)
             let point = tri.points[i]
             if (
+                !Closest.point ||
                 getDistance(...Closest.point.position, ...CursorPosition) > getDistance(...point.position, ...CursorPosition)
-            ) {
+            )
+            {
                 Closest.triangle = tri
                 Closest.point = point
             }
         }
     }
-    ctx.beginPath();
-    ctx.arc(...Closest.point.position, 5, 0, 2 * Math.PI);
-    ctx.stroke()
+    if (Closest.point)
+    {
+        ctx.beginPath();
+        ctx.arc(...Closest.point.position, 5, 0, 2 * Math.PI);
+        ctx.stroke()
+    }
 }
 
-function DrawTriangle(triangle) {
+function DrawTriangle(triangle)
+{
     // Draw stuff
     // Process each point
     ctx.beginPath();
-    for (let i in triangle.points) {
+    for (let i in triangle.points)
+    {
         point = triangle.points[i]
         let [x, y] = point.position
         ctx.lineTo(x, y);
@@ -250,7 +325,8 @@ function DrawTriangle(triangle) {
         ctx.font = "30px Arial";
 
         // Angle
-        if (i === 0) {
+        if (i === 0)
+        {
 
         }
         ctx.moveTo(x, y)
@@ -271,7 +347,8 @@ function DrawTriangle(triangle) {
 
         }
 
-        if (i === "2") {
+        if (i === "2")
+        {
             ctx.lineTo(...triangle.points[0].position)
         }
     }
@@ -279,55 +356,63 @@ function DrawTriangle(triangle) {
 }
 
 // Main Program
-function main() {
+function main()
+{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (CurrentTriangle.length <= 1) {
-        console.log("1 long")
-        return
-    }
-
-    let sides = []
-    let points = CurrentTriangle.points
-    let hypo = [0, 0]
-    // Get length of sides
-    for (let i = 0; i < points.length; i++) {
-        let [x1, y1] = points[i].position // current point
-        // console.log(points)
-        let [x2, y2] = points[(i === 2) ? 0 : i + 1].position // next point
-
-        let a = x1 - x2
-        let b = y1 - y2
-
-        let distance = Math.hypot(a, b)
-        if (distance > hypo[1]) {
-            hypo[0] = i
-            hypo[1] = distance
-        }
-        sides.push([
-            distance, // Calculate Distance
-            [(x1 + x2) / 2, (y1 + y2) / 2], // Calculate midpoint of side
-            i
-        ])
-    }
-
-    // We need 2 loops because need next index's value when setting current index's value.
-    for (let i = 0; i < sides.length; i++) {
-        let side = CurrentTriangle.points[i].side
-        let val = sides[(i === 2) ? 0 : i + 1] // next value
-
-        side.length = val[0]
-        side.midpoint = val[1]
-        side.hypo = val[2] === hypo[0]
-
-    }
-
-    CurrentTriangle.arrange() // Set triangles order. Shouldnt process anything until after this is done.
-    for (let i of Triangles)
+    if (CurrentTriangle)
     {
-        DrawTriangle(i)
+        if (CurrentTriangle.length <= 1)
+        {
+            console.log("1 long")
+            return
+        }
+
+        let sides = []
+        let points = CurrentTriangle.points
+        let hypo = [0, 0]
+        // Get length of sides
+        for (let i = 0; i < points.length; i++)
+        {
+            let [x1, y1] = points[i].position // current point
+            // console.log(points)
+            let [x2, y2] = points[(i === 2) ? 0 : i + 1].position // next point
+
+            let a = x1 - x2
+            let b = y1 - y2
+
+            let distance = Math.hypot(a, b)
+            if (distance > hypo[1])
+            {
+                hypo[0] = i
+                hypo[1] = distance
+            }
+            sides.push([
+                distance, // Calculate Distance
+                [(x1 + x2) / 2, (y1 + y2) / 2], // Calculate midpoint of side
+                i
+            ])
+        }
+
+        // We need 2 loops because need next index's value when setting current index's value.
+        for (let i = 0; i < sides.length; i++)
+        {
+            let side = CurrentTriangle.points[i].side
+            let val = sides[(i === 2) ? 0 : i + 1] // next value
+
+            side.length = val[0]
+            side.midpoint = val[1]
+            side.hypo = val[2] === hypo[0]
+
+        }
+
+        CurrentTriangle.arrange() // Set triangles order. Shouldnt process anything until after this is done.
+        for (let i of Triangles)
+        {
+            DrawTriangle(i)
+        }
     }
 
-    DrawSelector([CurrentTriangle])
+    DrawSelector(Triangles)
     // ShowWork(CurrentTriangle)
     // ShowWork(CurrentTriangle)
 }
@@ -336,26 +421,31 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext('2d');
 ctx.strokeStyle = 'red';
 ctx.lineWidth = 3;
-const CurrentTriangle = new Triangle()
+var CurrentTriangle = null
 var MouseUp_Interval_ID = 0
-var CursorPosition = [null, null]
-let Triangles = [CurrentTriangle]
+var CursorPosition = [300, 300]
+let Triangles = []
 
 // Input / Listeners
-window.onmousedown = function (e) {
+window.onmousedown = function (e)
+{
     // console.log(e)
-    if (e.button === 0) {
+    if (e.button === 0)
+    {
         MouseUp_Interval_ID = setInterval(SetClosest, 20)
-    } else {
+    } else
+    {
         CurrentTriangle.push(CursorPosition)
     }
 }
 
-canvas.onmouseup = function (e) {
+canvas.onmouseup = function (e)
+{
     clearInterval(MouseUp_Interval_ID)
 }
 
-document.onmousemove = function (e) {
+document.onmousemove = function (e)
+{
     GetCursorPosition(canvas, e)
 }
 
@@ -368,6 +458,10 @@ document.onkeyup = function (e)
         console.log(CurrentTriangle.right)
 
     }
+    else if (e.key === "d")
+    {
+        Triangles.push(new Triangle())
+    }
 }
 
 // Holding
@@ -376,6 +470,7 @@ document.onkeydown = function (e)
     if (e.key === " ")
     {
         MoveTriangle(CurrentTriangle)
+
 
     }
 }
